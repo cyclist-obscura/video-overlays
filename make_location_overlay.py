1dc00608e5ddcd9e43478a7d94051bc6a597d788
st599 <st599@zepler.net>
2019-09-12 19:06:42 +0100

    python file which creates location overlay with Location, Map, Date and up to Four Facts

---
+++ make_location_overlay.py
@@ -0,0 +1,152 @@
+################################################################################################
+#
+# file			   :		make_location_overlay.py
+# date			   : 		12/09/2019
+# author		   :		Cyclist Obscura
+# copyright		   :		(c) 2019
+# licence		   : 		GNU General Public License version 3
+# description	   :		Creates HTML and map PNG for Location
+# version          :        1.0
+#
+################################################################################################
+
+# Import required libraries
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import geotiler
+import dateutil.parser as parser
+import sys
+
+
+### Get Map File ###
+def getMapPngFile(lat,lon,filename):
+    lats = []
+    lons = []
+    # Small Scale
+    bbox = (lon-0.125, lat-0.0625, lon+0.125, lat+0.0625)
+    fig = plt.figure(figsize=(5, 5))
+    ax = plt.subplot(111)
+    #
+    # download background map using OpenStreetMap
+    #
+    mm = geotiler.Map(extent=bbox, zoom=11)
+    img = geotiler.render_map(mm)
+    #
+    # create basemap
+    #
+    map = Basemap(llcrnrlon=bbox[0], llcrnrlat=bbox[1],
+                urcrnrlon=bbox[2], urcrnrlat=bbox[3],
+                projection='merc', ax=ax)
+    map.imshow(img, interpolation='lanczos', origin='upper')
+    lats.append(lat)
+    lons.append(lon)
+    x, y = map(lons,lats)
+    map.scatter(x,y,125,marker='o',color='r')
+    plt.savefig(filename, bbox_inches='tight')
+    plt.close()
+
+### CREATE HTML FILE ###
+def createHtmlFile(htmlFile, locName, mapFile, dateStr, factStrs):
+    # Create filename
+    f= open(htmlFile,"w+")
+
+    # Write header
+    f.write("<!DOCTYPE html>\n")
+    f.write("<html>\n")
+    f.write("  <head>\n")
+    f.write("    <title>Location_template</title>\n")
+    f.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n")
+    f.write("    <meta charset=\"UTF-8\">\n")
+    f.write("    <link rel=\"stylesheet\" type=\"text/css\" href=\"obscura.css\">\n")
+    f.write("  </head>\n")
+    f.write("  <body>\n")
+
+    # Write Location
+    f.write("    <div class=\"fixed-location\">\n")
+    f.write("      <h1>" + locName + "</h1>\n")
+    f.write("    </div>\n")
+
+    # Write Date
+    f.write("    <div class=\"fixed-date\">\n")
+    f.write("      <h1>" + dateStr + "</h1>\n")
+    f.write("    </div>\n")
+
+    # Write Facts
+    for i in range(len(factStrs)):
+        classId = "fixed-fact" + str(i+1)
+        f.write("    <div class=\"" + classId + "\" align=\"right\">\n")
+        f.write("      <h3>" + factStrs[i] + "</h3>\n")
+        f.write("    </div>\n")
+
+    # Write map
+    mapUrl = "../" + mapFile
+    f.write("    <div id=\"mapid\" style=\"top: 50px; left: 1420px;\">\n")
+    f.write("      <img src=\"" + mapUrl + "\">\n")
+    f.write("    </div>\n")
+
+    # Write footer
+    f.write("  </body>\n")
+    f.write("</html>")
+
+    # Close filename
+    f.close()
+
+### MAIN FUNCTION ###
+def main():
+    print("Make Location Overlay v 1.0")
+    print("")
+
+    # Check Number of Args
+    if(len(sys.argv) < 5):
+        print("Too Few Arguments!!!!")
+    else:
+        # Parse Command Line
+        LOCATION_NAME = sys.argv[1]
+        LAT = sys.argv[2]
+        LON = sys.argv[3]
+        LATf = float(sys.argv[2])
+        LONf = float(sys.argv[3])
+        dateStr = sys.argv[4]
+        dateObj = parser.parse(dateStr, dayfirst = True)
+        formatDate = "%a %d %b %Y"
+        DATEHR = dateObj.strftime(formatDate)
+        factStrings = []
+        if (len(sys.argv) == 6):
+            factStrings.append(sys.argv[5])
+        if (len(sys.argv) == 7):
+            factStrings.append(sys.argv[5])
+            factStrings.append(sys.argv[6])
+        if (len(sys.argv) == 8):
+            factStrings.append(sys.argv[5])
+            factStrings.append(sys.argv[6])
+            factStrings.append(sys.argv[7])
+        if (len(sys.argv) == 9):
+            factStrings.append(sys.argv[5])
+            factStrings.append(sys.argv[6])
+            factStrings.append(sys.argv[7])
+            factStrings.append(sys.argv[8])
+
+        # Print Details
+        print("...Location Name: " + LOCATION_NAME)
+        print("...Latitude:      " + LAT)
+        print("...Longitude:     " + LON)
+        print("...Date:          " + DATEHR)
+        for i in range(len(factStrings)):
+            print("...Fact " + str(i) + ":        " + factStrings[i])
+
+        # Create Filenames
+        mapFile = "maps/" + LOCATION_NAME + ".png"
+        htmlFile = "html/" + LOCATION_NAME + ".html"
+        mapFileSafe = "_".join( mapFile.split() )
+        htmlFileSafe = "_".join( htmlFile.split() )
+
+        # Create Map
+        getMapPngFile(LATf,LONf,mapFileSafe)
+
+        # Create HTML file
+        createHtmlFile(htmlFileSafe, LOCATION_NAME, mapFileSafe, DATEHR, factStrings)
+
+
+### IF NOT USED AS LIBRARY ###
+if __name__ == '__main__':
+  main()
